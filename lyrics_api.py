45a84811ee56ae1a42ec0700d58ae5c76f6a6dd0
Amine Lemaizi <username@users.noreply.github.com>
2018-11-30 13:55:19 +0100

    Can support 2 498 requests

--- lyrics_api.py
+++ lyrics_api.py
@@ -1,8 +1,18 @@
 import requests
+from random import randint
 
 
 def lyrics(song, artist):
-	key = '26cc996c835cca5e9941d0c6a77ad8d1'
+	keys = [
+		'bffd099744c5812617813de69b93e2b0',
+		'16fce98709ab0cfda3d679a5a80d00aa',
+		'f65d821196427016661d6b80ce5bb2c9',
+		'911c39f720d7825fb2de2a98fe8905c1',
+		'591e236d68f408f61e6c7019bba857ac'
+	]
+	ind = randint(0, 4)
+	key = keys[ind]
+	#key = '26cc996c835cca5e9941d0c6a77ad8d1'
 	get_track = "https://api.musixmatch.com/ws/1.1/track.search?apikey={}&q_track={}&q_artist={}&f_has_lyrics=1&s_track_rating=desc".format(key, song, artist)
 
 	res = requests.get(get_track)
@@ -18,7 +28,7 @@ def lyrics(song, artist):
 		song_info['artist'] = track['artist_name']
 		song_info['genre'] = track['primary_genres']['music_genre_list'][0]['music_genre']['music_genre_name']
 
-		get_lyrics = "https://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=26cc996c835cca5e9941d0c6a77ad8d1&track_id={}".format(song_info['id'])
+		get_lyrics = "https://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey={}&track_id={}".format(key, song_info['id'])
 		res = requests.get(get_lyrics)
 		res_data = res.json()
 
